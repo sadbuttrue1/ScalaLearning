3f41cb38433525542574e0c689b4dbc3479310f1
Eugene Aslanov <aslanov.ev@gmail.com>
2016-04-13 23:23:32 +0400

    Add exercises from the first chapter

---
+++ Scala for the Impatient/src/tk/sadbuttrue/chapter/one/Main.scala
@@ -0,0 +1,35 @@
+package tk.sadbuttrue.chapter.one
+
+import math._
+import BigInt._
+import util._
+
+/**
+  * Created by true on 13/04/16.
+  */
+object Main extends App {
+  //  Task 2
+  println(3)
+  println(pow(sqrt(3), 2))
+
+  //  Task 4
+  println("crazy" * 3)
+
+  //  Task 5
+  println(10 max 2)
+
+  //  Task 6
+  println(BigInt(2) pow 1024)
+
+  //  Task 7
+  println(probablePrime(100, Random))
+
+  //  Task 8
+  println(probablePrime(100, Random).toString(36))
+
+  //  Task 9
+  val hello = "Hello"
+  println(hello.head)
+  println(hello.last)
+
+}
